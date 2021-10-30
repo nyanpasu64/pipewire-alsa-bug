b576e05ba896d140bc144b5a7c7a9785923e34cb
nyanpasu64 <nyanpasu64@tuta.io>
2021-10-29 17:25:16 -0700

    init

--- alsa.cpp
+++ alsa.cpp
@@ -12,7 +12,7 @@
 #define max(x,y) (((x)>(y))?(x):(y))
 #define min(x,y) (((x)<(y))?(x):(y))
 
-double freq = 100;
+double freq = 256;
 double volume = 0.2;
 
 float mixbuffer[4096*256];
@@ -37,10 +37,10 @@ int main()
 //    if(snd_pcm_open(&device, "default", SND_PCM_STREAM_PLAYBACK, SND_PCM_NONBLOCK) < 0)
     if(snd_pcm_open(&device, "default", SND_PCM_STREAM_PLAYBACK, 0) < 0)
         return puts("Failed to open ALSA device"), 0;
-       
+
     if(snd_pcm_hw_params_malloc(&parameters) < 0)
         return puts("Failed to allocate device parameter structure"), 0;
-             
+
     if(snd_pcm_hw_params_any(device, parameters) < 0)
         return puts("Failed to init device parameter structure"), 0;
 
@@ -57,23 +57,23 @@ int main()
     int channels = 2;
     if(snd_pcm_hw_params_set_channels(device, parameters, channels) < 0)
         return puts("Failed to set channel count"), 0;
-   
+
     printf("actual sample rate %d\n", samplerate);
-   
+
     snd_pcm_uframes_t period;
     int dir;
-   
+
     if(snd_pcm_hw_params_get_period_size_min(parameters, &period, &dir) < 0)
         return puts("Failed to get period size"), 0;
-   
+
     printf("min period size: %lu\n", period);
-   
+
     if(snd_pcm_hw_params_set_period_size(device, parameters, period, dir) < 0)
         return puts("Failed to set period size to the minimum provided by it"), 0;
-   
+
     snd_pcm_uframes_t samples = max(period, 1024);
     printf("pretend buffer size is %lu\n", samples);
-   
+
     if(snd_pcm_hw_params_set_buffer_size_near(device, parameters, &samples) < 0)
         return puts("Failed to set buffer size to something near our sample window"), 0;
 
@@ -84,16 +84,26 @@ int main()
 
     if (snd_pcm_prepare(device) < 0)
         return puts("Failed to prepare device"), 0;
-   
+
     auto buffer_size = snd_pcm_avail_update(device);
     if(buffer_size < 0)
         return puts("Error when measuring approximate buffer size"), 0;
-   
+
     printf("apparent buffer size is %lu\n", buffer_size);
-   
+
     uint64_t t = 0;
-    while(t < 100000)
+    uint64_t drains = 0;
+    while(t < 100'000)
     {
+//        {
+//            auto count = t / 10'000;
+//            if (count > drains)
+//            {
+//                snd_pcm_drain(device);
+//                snd_pcm_prepare(device);
+//                drains = count;
+//            }
+//        }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         auto avail = snd_pcm_avail_update(device);
         while(avail == -EPIPE or avail == -ESTRPIPE)
@@ -101,20 +111,24 @@ int main()
             snd_pcm_recover(device, avail, 0);
             avail = snd_pcm_avail_update(device);
         }
+        printf("avail = %ld\n", avail);
+        if (avail <= 0) {
+            continue;
+        }
         int padding = buffer_size - avail;
-       
+
         int render = min(uint64_t(buffer_size), samples) - padding;
-       
+
         if(render > 0)
         {
             mix_sine(mixbuffer, render, samplerate, 2, t);
             t += render;
-           
+
             for(int i = 0; i < render*2; i++)
                 outbuffer[i] = round(mixbuffer[i]*0x7FFF);
-           
+
             auto written = snd_pcm_writei(device, outbuffer, render);
-           
+
             while(written == -EPIPE or written == -ESTRPIPE)
             {
                 snd_pcm_recover(device, written, 0);
