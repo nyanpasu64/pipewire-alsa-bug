56e842992201efe4a4e8e11a221a2a8ee9ce8a61
nyanpasu64 <nyanpasu64@tuta.io>
2021-10-30 17:08:37 -0700

    no hang

--- pcm_min.cpp
+++ pcm_min.cpp
@@ -8,13 +8,18 @@
 static char const *device = "default";            /* playback device */
 unsigned char buffer[16*1024];              /* some random data */
 
-int drain = 0;
-
-static void fn() {
+int main(void)
+{
     using namespace std::chrono_literals;
 
     int err;
+    unsigned int i;
     snd_pcm_t *handle;
+    snd_pcm_sframes_t frames;
+
+    for (i = 0; i < sizeof(buffer); i++)
+        buffer[i] = random() & 0xff;
+
     if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
         printf("Playback open error: %s\n", snd_strerror(err));
         exit(EXIT_FAILURE);
@@ -30,22 +35,7 @@ static void fn() {
         exit(EXIT_FAILURE);
     }
 
-    snd_pcm_sframes_t frames;
-
     while (1) {
-        // If you don't sleep, the main thread *never* gets to acquire the mutex.
-        std::this_thread::sleep_for(5ms);
-
-        snd_pcm_state_t state = snd_pcm_state(handle);
-        std::cerr << "state " << state << "\n";
-//        if (state != SND_PCM_STATE_PREPARED && state != SND_PCM_STATE_RUNNING) {
-        if (drain % 4 == 0) {
-            snd_pcm_drain(handle);
-            std::cerr << "new state " << snd_pcm_state(handle) << "\n";
-            snd_pcm_prepare(handle);
-        }
-        drain = (drain + 1) % 4;
-
         frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
         if (frames < 0)
             frames = snd_pcm_recover(handle, frames, 0);
@@ -55,13 +45,16 @@ static void fn() {
         }
         if (frames > 0 && frames < (long)sizeof(buffer))
             printf("Short write (expected %li, wrote %li)\n", (long)sizeof(buffer), frames);
+
+        snd_pcm_drain(handle);
+        snd_pcm_state(handle);
+        snd_pcm_prepare(handle);
     }
-}
 
-int main(void)
-{
-    for (size_t i = 0; i < sizeof(buffer); i++)
-        buffer[i] = random() & 0xff;
-
-    fn();
+    /* pass the remaining samples, otherwise they're dropped in close */
+    err = snd_pcm_drain(handle);
+    if (err < 0)
+        printf("snd_pcm_drain failed: %s\n", snd_strerror(err));
+    snd_pcm_close(handle);
+    return 0;
 }
