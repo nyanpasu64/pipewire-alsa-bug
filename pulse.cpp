5066f7d6b6a37958a2850c6739f2b1180346f157
nyanpasu64 <nyanpasu64@tuta.io>
2021-11-05 04:25:45 -0700

    pulse sine

--- pulse.cpp
+++ pulse.cpp
@@ -3,18 +3,41 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <math.h>
 
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
-#define BUFSIZE 1024
+constexpr size_t BUFSIZE = 1024;
+constexpr size_t NCHAN = 2;
+
+double freq = 256;
+double volume = 0.2;
+
+float mixbuffer[BUFSIZE * NCHAN];
+int16_t outbuffer[BUFSIZE * NCHAN];
+static void mix_sine(float * buffer, uint64_t count, uint64_t samplerate, uint64_t channels, uint64_t time)
+{
+    double nyquist = samplerate/2.0;
+    for(uint64_t i = 0; i < count; i++)
+    {
+        for(uint64_t c = 0; c < channels; c++)
+        {
+            buffer[i*channels + c] = sin((i+time)*M_PI/nyquist*freq)*volume;
+        }
+    }
+}
+
+#define perr(...) fprintf(stderr, __VA_ARGS__)
 
 int main(int argc, char*argv[]) {
 
+    uint32_t rate = 44100;
+
     /* The Sample format to use */
     static const pa_sample_spec ss = {
-        .format = PA_SAMPLE_S16LE,
-        .rate = 44100,
+        .format = PA_SAMPLE_S16NE,
+        .rate = rate,
         .channels = 2
     };
 
@@ -22,58 +45,26 @@ int main(int argc, char*argv[]) {
     int ret = 1;
     int error;
 
-    /* replace STDIN with the specified file if needed */
-    if (argc > 1) {
-        int fd;
-
-        if ((fd = open(argv[1], O_RDONLY)) < 0) {
-            fprintf(stderr, __FILE__": open() failed: %s\n", strerror(errno));
-            goto finish;
-        }
-
-        if (dup2(fd, STDIN_FILENO) < 0) {
-            fprintf(stderr, __FILE__": dup2() failed: %s\n", strerror(errno));
-            goto finish;
-        }
-
-        close(fd);
-    }
-
     /* Create a new playback stream */
     if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
         goto finish;
     }
 
-    for (;;) {
-        uint8_t buf[BUFSIZE];
-        ssize_t r;
+    for (uint64_t t = 0;;) {
+        mix_sine(mixbuffer, BUFSIZE, rate, NCHAN, t);
+        t += BUFSIZE;
 
-#if 0
-        pa_usec_t latency;
-
-        if ((latency = pa_simple_get_latency(s, &error)) == (pa_usec_t) -1) {
-            fprintf(stderr, __FILE__": pa_simple_get_latency() failed: %s\n", pa_strerror(error));
-            goto finish;
-        }
-
-        fprintf(stderr, "%0.0f usec    \r", (float)latency);
-#endif
-
-        /* Read some data ... */
-        if ((r = read(STDIN_FILENO, buf, sizeof(buf))) <= 0) {
-            if (r == 0) /* EOF */
-                break;
-
-            fprintf(stderr, __FILE__": read() failed: %s\n", strerror(errno));
-            goto finish;
-        }
+        for(size_t i = 0; i < BUFSIZE * NCHAN; i++)
+            outbuffer[i] = round(mixbuffer[i]*0x7FFF);
 
         /* ... and play it */
-        if (pa_simple_write(s, buf, (size_t) r, &error) < 0) {
+        perr("writing...\n");
+        if (pa_simple_write(s, outbuffer, BUFSIZE * NCHAN * sizeof(int16_t), &error) < 0) {
             fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
             goto finish;
         }
+        perr("done.\n");
     }
 
     /* Make sure that every single sample was played */
