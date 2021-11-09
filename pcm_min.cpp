407551d7999f4bca30cb504242b18f7d1e34316f
nyanpasu64 <nyanpasu64@tuta.io>
2021-11-09 13:36:22 -0800

    Make pcm_min.cpp run on hw devices

--- pcm_min.cpp
+++ pcm_min.cpp
@@ -1,12 +1,17 @@
 #include <alsa/asoundlib.h>
 
 #include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <thread>
 
 static char const *device = "default";            /* playback device */
-unsigned char buffer[16*1024];              /* some random data */
+
+constexpr size_t FRAMES = 16 * 1024;
+constexpr size_t CHANNELS = 2;
+
+int16_t buffer[FRAMES * CHANNELS];              /* some random data */
 
 int main(void)
 {
@@ -17,17 +22,17 @@ int main(void)
     snd_pcm_t *handle;
     snd_pcm_sframes_t frames;
 
-    for (i = 0; i < sizeof(buffer); i++)
-        buffer[i] = random() & 0xff;
+    for (auto & value : buffer)
+        value = random();
 
     if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
         printf("Playback open error: %s\n", snd_strerror(err));
         exit(EXIT_FAILURE);
     }
     if ((err = snd_pcm_set_params(handle,
-                      SND_PCM_FORMAT_U8,
+                      SND_PCM_FORMAT_S16,
                       SND_PCM_ACCESS_RW_INTERLEAVED,
-                      1,
+                      CHANNELS,
                       48000,
                       1,
                       500000)) < 0) {   /* 0.5sec */
@@ -39,15 +44,15 @@ int main(void)
 
     while (1) {
         printf("before writei %d\n", snd_pcm_state(handle));
-        frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
+        frames = snd_pcm_writei(handle, buffer, FRAMES);
         if (frames < 0)
             frames = snd_pcm_recover(handle, frames, 0);
         if (frames < 0) {
-            printf("snd_pcm_writei failed: %s\n", snd_strerror(frames));
+            printf("snd_pcm_writei failed: %d %s\n", frames, snd_strerror(frames));
             break;
         }
-        if (frames > 0 && frames < (long)sizeof(buffer))
-            printf("Short write (expected %li, wrote %li)\n", (long)sizeof(buffer), frames);
+        if (frames > 0 && frames < (long)FRAMES)
+            printf("Short write (expected %li, wrote %li)\n", FRAMES, frames);
 
         if (ctr == 0) {
             ctr += 1;
